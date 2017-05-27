1ae5c901ce6fd849e2f05f3605f39e2eb24ec02e
Hugo <hugo-chabin@laposte.net>
2017-05-27 12:09:41 +0200

    Cas insertion à la racine traité

--- main.c
+++ main.c
@@ -8,9 +8,12 @@ int main()
     Arbre* a = initABR();
     printf("init\n");
     Sommet* s1 = creerSommet(6);
+    Sommet* s2 = creerSommet(4);
+    Sommet* s3 = creerSommet(8);
     printf("init\n");
     afficherSommet(s1);
     int b = insererSommet(a, s1);
+    int c = insererSommet(a, s2);
     printf("%d\n", b);
 
 
--- tp4.c
+++ tp4.c
@@ -20,7 +20,7 @@ Sommet* creerSommet(int cle){
 }
 
 void afficherSommet(Sommet* s){
-    printf("%d", s->val);
+    printf("%d\n", s->val);
 }
 
 /*int vide(Arbre* a){
@@ -31,12 +31,34 @@ void afficherSommet(Sommet* s){
 
 int insererSommet(Arbre* a, Sommet* s){
     //Insertion si arbre vide
-    if(a->racine==NULL){
-        printf("test\n");
+    if(!a->racine){
         a->racine=s;
+        return 1;
     }
 
     //Insertion en feuille
-
+    int cle = s->val;
+    Sommet* temp = a->racine;
+    afficherSommet(temp);
+    while(temp->droit!=NULL && temp->droit!=NULL){
+        if(cle<temp->val){
+            printf("premier if\n");
+            temp=temp->gauche;
+        }
+        else if(cle>temp->val){
+            temp=temp->droit;
+        }
+        else if(cle==temp->val){
+            return 0;
+        }
+    }
+    if(temp->val>cle){
+        s->pere = temp;
+        temp->droit = s;
+    }
+    else{
+        s->pere = temp;
+        temp->gauche = s;
+    }
     return 1;
 }
